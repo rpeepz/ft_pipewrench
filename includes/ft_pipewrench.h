a3166e03c3173b31796070585142520da4c52399
Robert Papagna <rpapagna@e1z2r8p15.42.us.org>
2019-04-11 22:19:44 -0700

    necessary

---
+++ includes/ft_pipewrench.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pipewrench.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/11 18:05:48 by rpapagna          #+#    #+#             */
+/*   Updated: 2019/04/11 22:17:10 by rpapagna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PIPEWRENCH_H
+# define FT_PIPEWRENCH_H
+
+# include "../libft/includes/libft.h"
+# include <stdarg.h>
+
+# define IF_THEN(X, Y) if(X) {(Y);}
+# define KEY '-'
+
+int		ft_pipewrench(char *str, ...);
+
+#endif
